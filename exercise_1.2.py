e6db7cf80e11c506a885e372b208bc5dccde5822
Dave00007 <butu95@wp.pl>
2017-12-09 14:05:07 +0100

    "Added exercise 1.1-1.2"

---
+++ exercise_1.2.py
@@ -0,0 +1,6 @@
+'''
+Napisz skrypt, który pyta o imie, nazwisko i rok urodzenia (powinny byc podane w jednej linii)
+'''
+
+name, surname, dateOfBirth = input("Enter your name, surname, date of birth in one line: ").split()
+print ('Your name: ' + name + ', surname: ' + surname + ', date of birth: ' + dateOfBirth)
